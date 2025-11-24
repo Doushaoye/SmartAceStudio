3f476af5bb25727b8d98fac4eef18715b3140d58
Lan Hong <hthuy99o3o@gmail.com>
2025-11-24 17:27:23 +0000

    把外部 div 容器的尺寸不需要增加这么多，只需要增加1.5倍

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 export function Logo() {
   return (
-    <div className="flex size-36 items-center justify-center rounded-lg text-primary-foreground">
+    <div className="flex size-14 items-center justify-center rounded-lg text-primary-foreground">
       <Image 
         src="https://free.picui.cn/free/2025/11/25/692494766a07f.png"
         alt="智装侠 Logo"
-        width={144}
-        height={144}
+        width={56}
+        height={56}
         className="object-contain rounded-md"
       />
     </div>
