7a86d2649b83ca5c7c8b6d6c1eb6de6973067ee6
Lan Hong <hthuy99o3o@gmail.com>
2025-11-25 20:26:04 +0000

    解决一下上述问题吧

---
+++ src/next.config.ts
@@ -0,0 +1,57 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https://',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'free.picui.cn',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '4mb',
+    },
+  },
+  allowedDevOrigins: ['*.cloudworkstations.dev'],
+  async headers() {
+    return [
+        {
+            source: "/:path*",
+            headers: [
+                { key: "x-action-timeout", value: "120" },
+            ],
+        },
+    ];
+  },
+};
+
+export default nextConfig;
