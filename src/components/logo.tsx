a3d8223f249c5fdc6b2e59633ec2b003d8cc0ce7
Lan Hong <hthuy99o3o@gmail.com>
2025-11-25 10:09:49 +0000

    Image with src "https://free.picui.cn/free/2025/11/25/692495e34d1bf.png"

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,12 +2,11 @@ import Image from 'next/image';
 
 export function Logo() {
   return (
-    <div className="flex size-42 items-center justify-center rounded-lg text-primary-foreground">
+    <div className="relative w-16 h-16 flex items-center justify-center rounded-lg text-primary-foreground">
       <Image 
         src="https://free.picui.cn/free/2025/11/25/692495e34d1bf.png"
         alt="智装侠 Logo"
-        width={168}
-        height={168}
+        fill
         className="object-contain rounded-md"
       />
     </div>
